7e070b782151aab51dbc7388d2288d687ab5924a
amntllhz <amanatullah1904@gmail.com>
2026-09-13 13:45:01 +0700

    sprint 1: setup docker postgre, prisma, betterauth, route, proxy

---
+++ prisma/migrations/20260912055527_remove_username_password/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `user` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "user_username_key";
+
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "password",
+DROP COLUMN "username";
